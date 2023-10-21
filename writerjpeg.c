5fe4f35c0cca3884e5e7b156402ed3ae949e078f
crojas <candyrojas579@gmail.com>
2023-10-20 22:19:32 -0600

    Actualizaciòn de funcion con array de pixeles JPEG y funcion que guarda la imagen JPEG rotada.

--- writerjpeg.c
+++ writerjpeg.c
@@ -3,80 +3,63 @@
 #include <jpeglib.h>
 #include <jerror.h>
 
-extern int width;
-extern int height;
-extern int numChannels;
+// Función para obtener una matriz de píxeles de la imagen.
+unsigned char* JPEGMatrix(const ImageData *imageData) {
+unsigned char *pixelArray;
+int imageSize = imageData->width * imageData->height * imageData->numChannels;
 
-int writeJPEGImage(const char *filename, unsigned char *newimageData, int width, int height, int numChannels) {
+//Asigna memoria para la matriz.
+pixelArray = (unsigned char *)malloc(imageSize * sizeof(unsigned char));
+
+for (int i = 0; i < imageSize; i++) {
+pixelArray[i] = imageData->data[i];
+}
+
+return pixelArray; //Retorna matriz de pixeles.
+}
+
+// Función para guardar una imagen JPEG.
+void writeJPEGImage(const char *filename, const unsigned char *rotatedimage, int width, int height) {
 struct jpeg_compress_struct cinfo;
 struct jpeg_error_mgr jerr;
-FILE *outfile = NULL;
 
-// Inicializa la estructura de manejo de errores.
+int numChannels=3;
+int quality=95;
+
+//Abre el archivo de salida en modo lectura binaria.
+FILE *outfile = fopen(filename, "wb");
+if (!outfile) {
+fprintf(stderr, "No se pudo abrir el archivo de salida: %s\n", filename);
+return;
+}
 cinfo.err = jpeg_std_error(&jerr);
 
-// Inicializa la estructura de compresión.
+//Inicia estructura de compresion.
 jpeg_create_compress(&cinfo);
-
-// Abre el archivo de salida en modo escritura binaria.
-if ((outfile = fopen(filename, "wb")) == NULL) {
-fprintf(stderr, "No se pudo abrir el archivo de salida %s\n", filename);
-
-if (outfile != NULL) {
-fclose(outfile); // Cierra el archivo si se abrió antes del error.
-}
-// Libera la estructura de compresión.
-jpeg_destroy_compress(&cinfo);
-return 0; // Error
-}
-
-// Configura el archivo de salida para JPEG.
 jpeg_stdio_dest(&cinfo, outfile);
 
-// Configura caracteristicas de la imagen.
+//Configura caracteristicas de la imagen.
 cinfo.image_width = width;
 cinfo.image_height = height;
 cinfo.input_components = numChannels;
-cinfo.in_color_space = JCS_RGB; // Espacio de color RGB.
+cinfo.in_color_space = JCS_RGB;
 
-// Configura los valores por defecto para la compresión JPEG.
 jpeg_set_defaults(&cinfo);
+jpeg_set_quality(&cinfo, quality, TRUE);
 
-// Inicia el proceso de compresión.
 jpeg_start_compress(&cinfo, TRUE);
 
 JSAMPROW row_pointer[1];
 int row_stride = width * numChannels;
 
-// Itera a través de las líneas de escaneo de la imagen y escribe cada línea.
 while (cinfo.next_scanline < cinfo.image_height) {
-row_pointer[0] = &newimageData[cinfo.next_scanline * row_stride];
-if (jpeg_write_scanlines(&cinfo, row_pointer, 1) != 1) {
-fprintf(stderr, "Error al escribir líneas de escaneo\n");
-
-//Cierra el archivo en caso de error.
-fclose(outfile);
-
-// Libera la estructura de compresión.
-jpeg_destroy_compress(&cinfo);
-return 0; // Error
-}
+row_pointer[0] = (JSAMPROW)&rotatedimage[cinfo.next_scanline * row_stride];
+jpeg_write_scanlines(&cinfo, row_pointer, 1);
 }
 
-// Finaliza el proceso de compresión.
+//Termina compresion y se liberan recursos.
 jpeg_finish_compress(&cinfo);
-
-// Cierra el archivo de salida.
-if (fclose(outfile) == EOF) {
-fprintf(stderr, "Error al cerrar el archivo de salida\n");
-
-//Libera la estructura de compresiòn.
+fclose(outfile);
 jpeg_destroy_compress(&cinfo);
-return 0; // Error
 }
 
-// Libera la estructura de compresión.
-jpeg_destroy_compress(&cinfo);
-
-return 1; // Éxito
-}
